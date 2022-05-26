0666d97c00710e1b5293098d881ce044b8cdf69e
peppapig450 <nbgenius1@gmail.com>
2022-05-26 13:26:51 -0700

    write first part of customer class (end of work session)

---
+++ Customer.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String uuid = "";
+
+    private byte accountPinHash[];
+    private ArrayList<Account> customerAccounts;
+
+    // constructor for Customer object
+    public Customer(String firstName, String lastName, String pin, Bank bank) {
+
+    }
+}
