6c7223bfb112316cb91725dc781373888cec8095
peppapig450 <nbgenius1@gmail.com>
2022-05-26 21:13:22 -0700

    write constructor for customer class

--- Customer.java
+++ Customer.java
@@ -1,5 +1,12 @@
 import java.util.ArrayList;
 
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.KeyStore.SecretKeyEntry;
+
 public class Customer {
     private String firstName;
     private String lastName;
@@ -8,8 +15,39 @@ public class Customer {
     private byte accountPinHash[];
     private ArrayList<Account> customerAccounts;
 
-    // constructor for Customer object
+    // create new customer with given name, pin, and bank branch
     public Customer(String firstName, String lastName, String pin, Bank bank) {
 
+        // set user's name
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        // store the pin's salted PBKDF2 hash instead of original, for security reasons
+        this.accountPinHash = getAccountPinHash(pin);
+
+        // get a new, unique universal unique ID for the user
+        this.uuid = bank.getNewUserUUID();
+
+        // create empty list of accounts
+        customerAccounts = new ArrayList<Account>();
+
+        // print log message
+        System.out.printf("New user %s, %s with ID %s created.\n",
+                lastName, firstName, uuid);
+    }
+
+    // method to get account pin as hash for security purposes
+    public byte[] getAccountPinHash(String pin) {
+        // Generate salt for hash
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+
+        KeySpec spec = new PBEKeySpec(pin.toCharArray(), salt, 65536, 128);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+        byte[] hash = factory.generateSecret(spec).getEncoded();
+
+        return hash;
     }
 }
