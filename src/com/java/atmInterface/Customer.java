d8802754b729611e2d808d81c27734b1be45f3e8
peppapig450 <nbgenius1@gmail.com>
2022-05-28 13:51:16 -0700

    Add the code in the customer and bank classes to validate a user's pin for logging into the system

--- src/com/java/atmInterface/Customer.java
+++ src/com/java/atmInterface/Customer.java
@@ -1,18 +1,14 @@
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.KeySpec;
+package com.java.atmInterface;
+
+import com.java.atmInterface.security.PinHash;
+
 import java.util.ArrayList;
-
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import java.security.spec.InvalidKeySpecException;
-
 public class Customer {
     private String firstName;
     private String lastName;
     private String uuid = "";
 
-    private byte accountPinHash[];
+    private String accountPinHash;
     private ArrayList<Account> customerAccounts;
 
     // create new customer with given name, pin, and bank branch
@@ -23,7 +19,7 @@ public class Customer {
         this.lastName = lastName;
 
         // store the pin's salted PBKDF2 hash instead of original, for security reasons
-        this.accountPinHash = getAccountPinHash(pin);
+        this.accountPinHash = PinHash.generateHash(pin);
 
         // get a new, unique universal unique ID for the user
         this.uuid = bank.getNewUserUUID();
@@ -36,19 +32,8 @@ public class Customer {
                 lastName, firstName, uuid);
     }
 
-    // method to get account pin as hash for security purposes
-    public byte[] getAccountPinHash(String pin) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        // Generate salt for hash
-        SecureRandom sr = new SecureRandom();
-        byte[] salt = new byte[16];
-        sr.nextBytes(salt);
-
-        // initialize key factory 
-        PBEKeySpec spec = new PBEKeySpec(pin.toCharArray(), salt, 1000, 512);
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-
-        byte[] hash = factory.generateSecret(spec).getEncoded();
-
-        return hash;
+    public String getAccountPinHash() {
+        return this.accountPinHash;
     }
+
 }
