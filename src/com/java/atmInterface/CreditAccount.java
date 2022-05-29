eaa2e2072e02b364741b0ac8f57c5d0807845c71
peppapig450 <nbgenius1@gmail.com>
2022-05-29 10:35:56 -0700

    add extension of account class for credit accounts

---
+++ src/com/java/atmInterface/CreditAccount.java
@@ -0,0 +1,23 @@
+package com.java.atmInterface;
+
+import java.util.ArrayList;
+
+public class CreditAccount extends Account {
+
+    private double overDraftLimit;
+
+    // constructor for a credit card account object
+    public CreditAccount(String name, Customer holder, Bank bank, double overDraftLimit) {
+        super(name, holder, bank);
+
+        this.overDraftLimit = overDraftLimit;
+
+    }
+
+    public double getBalance() {
+        double balance = 0;
+        balance = overDraftLimit;
+        return balance;
+    }
+     
+}
