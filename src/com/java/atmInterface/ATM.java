ff0f4635390a0724ad266549a6aa1bf79d3a52fd
peppapig450 <nbgenius1@gmail.com>
2022-05-30 16:07:48 -0700

    finish everything for now. time to test.

--- src/com/java/atmInterface/ATM.java
+++ src/com/java/atmInterface/ATM.java
@@ -6,7 +6,7 @@ import java.security.spec.InvalidKeySpecException;
 
 
 public class ATM implements Overdraft {
-    public static void main(String[] args) throws FileNotFoundException, NoSuchAlgorithmException, InvalidKeySpecException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException {
         Scanner sc = new Scanner(System.in);
         Bank theBank = new Bank("Golden Horizon Bank");
         Customer defaultUser = theBank.addUser("Harry", "Patterson", "0000");
@@ -56,7 +56,7 @@ public class ATM implements Overdraft {
                 System.out.print("| Enter user ID: ");
                 userID = sc.nextLine();
                 System.out.print("| Enter pin: ");
-                userID = sc.nextLine();
+                pin = sc.nextLine();
 
                 // attempt to get the customer object corresponding to the entered ID and pin combo
                 try {
@@ -125,7 +125,7 @@ public class ATM implements Overdraft {
                     ATM.transferFunds(user, sc);
                     break;
                 case 4:
-                    ATM.showTransferHistory(user, sc); // print all transactions for user on screen
+                    ATM.showTransactionHistory(user, sc); // print all transactions for user on screen
                 case 5:
                     System.out.println("Please contact your branch to discuss available overdraft options. \n"); // overdraft options
                     ATM.overdraftMenu(user, sc);
@@ -197,14 +197,121 @@ public class ATM implements Overdraft {
             } while (amount < 0 || amount > accountBal); // loop until valid amount is entered
             
             
-            user.addAccountTransaction(fromAccount, amount, String.format("Transfer to account %s",
+            user.addAccountTransaction(fromAccount, -1 * amount, String.format("Transfer to account %s",
                 user.getAccountUUID(toAccount)));
 
-            user.addAccountTransaction(toAccount, amount, String.format("Transfer from ac "));
+            user.addAccountTransaction(toAccount, -1 * amount, String.format("Transfer from account %s", 
+                user.getAccountUUID(fromAccount)));
+        }
+
+        // process a withdrawl of funds from an account
+        public static void withdrawFunds(Customer user, Scanner sc) {
+            int withdrawAccount;
+            double amount, accountBal;
+            String note;
+
+            if (user.numAccounts() != 1) {
+                // get account to withdraw from
+                do {
+                    System.out.printf("Enter the number (1-%d) of the account to withdraw from: ", 
+                        user.numAccounts());
+                    withdrawAccount = sc.nextInt();
+                    // check if the account number is valid
+                    if (withdrawAccount < 0 || withdrawAccount >= user.numAccounts()) {
+                        System.out.println("Invalid account. Please try again.");
+                    }
+                } while (withdrawAccount < 0 || withdrawAccount >= user.numAccounts()); // loop until valid response is given
+            } else {
+                withdrawAccount = 1;
+            }
+
+            accountBal = user.getAccountBalance(withdrawAccount);
+            
+            // get amount to transfer
+            do {
+                System.out.printf("Enter the amount to withdraw (max $%.02f): $", accountBal);
+                amount = sc.nextDouble();
+                // make sure amount is greater than 0 and not exceeding the account's balance
+                if (amount > 0) {
+                    System.out.println("Amount must be greater than zero.");
+                } else if (amount > accountBal) {
+                    System.out.printf("Amount must not be greater than balance of $%.02f.\n", accountBal);
+                }
+            } while (amount < 0 || amount > accountBal); // loop until valid response is given
+
+            sc.nextLine();
+
+            // get a note
+            System.out.print("Enter a note for the transaction: ");
+            note = sc.nextLine();
+
+            user.addAccountTransaction(withdrawAccount, -1 * amount, note);
+        }
+
+        public static void depositFunds(Customer user, Scanner sc) {
+            int toAccount;
+            double amount;
+            String note;
+
+            if (user.numAccounts() != 1) {
+                do {
+                    System.out.print("Enter the number (1-%d) of the account to deposit to: " + user.numAccounts());
+                    toAccount = sc.nextInt() - 1;
+
+                    // check if entered account number is valid
+                    if (toAccount < 0 || toAccount >= user.numAccounts()); {
+                        System.out.println("Invalid account. Please try again.");
+                    }
+                } while (toAccount < 0 || toAccount >= user.numAccounts()); // loop uuntil we get a valid response
+            } else {
+                toAccount = 1;
+            }
+
+            // get amount to transfer
+            do {
+                System.out.println("Enter the amount to deposit: $");
+                amount = sc.nextDouble();
+                if (amount < 0) {
+                    System.out.println("Amount must be greater than zero.");
+                }
+            } while (amount < 0); // loop until a valid amount is entered
+
+            sc.nextLine();
+
+            // get a note
+            System.out.print("Enter a Note for your deposit: "); 
+            note = sc.nextLine();
+
+            // do the deposit
+            user.addAccountTransaction(toAccount, amount, note);
+        }
+
+        public static void showTransactionHistory(Customer user, Scanner sc) {
+            int theAccount = 0;
+
+            if (user.numAccounts() != 1) {
+                do {
+                    System.out.printf("Entere the number (1-%d) of the account\nwhose transactions you want to see: ", user.numAccounts());
+                    theAccount = sc.nextInt() - 1;
+                    if (theAccount < 0 || theAccount >= user.numAccounts()) {
+                        System.out.println("Invalid account. Please try again.");
+                    }
+                } while (theAccount < 0  || theAccount >= user.numAccounts()); // loop until a valid response is entered
+            } else {
+                theAccount = 1;
+            }
+
+            user.displayAccountHistory(theAccount);
         }
 
 
-                    
+        public void payOverDraft() {
+
+        }
+
+        public void checkOverDraftRate() {
+
+        }
 
 
 }
